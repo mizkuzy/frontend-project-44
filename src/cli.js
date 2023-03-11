5e7ff9a1aed8acd84c0d601aa059d4f30fb16d40
lrudkova <puoiosiuvkotalhzwkmcvvxvancqbnrhm4ttwye5amlbfdbictrq>
2023-03-11 17:54:25 +0200

    Get username from keyboard

---
+++ src/cli.js
@@ -0,0 +1,14 @@
+import readlineSync from 'readline-sync';
+
+
+export function printIt(input) {
+  console.log(input);
+}
+
+export default function run() {
+  printIt('Welcome to the Brain Game!');
+
+  const userName = readlineSync.question('May I have your name? ');
+
+  printIt(`Hello, ${userName}`);
+}
