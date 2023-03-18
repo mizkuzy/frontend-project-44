8367b114ee96ec67448bf8689f70ade266ab0d3c
lrudkova <puoiosiuvkotalhzwkmcvvxvancqbnrhm4ttwye5amlbfdbictrq>
2023-03-18 15:43:58 +0200

    Add dot in the Prime Game description

--- src/games/is-prime-number.js
+++ src/games/is-prime-number.js
@@ -36,7 +36,7 @@ export function getQuestion() {
 }
 
 export default function startPrimeNumberGame() {
-  const gameDescription = 'Answer "yes" if given number is prime. Otherwise answer "no"';
+  const gameDescription = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
   run(gameDescription, getQuestion, getAnswer);
 }
